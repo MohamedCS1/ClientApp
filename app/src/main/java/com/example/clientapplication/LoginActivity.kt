77dc441d284a468641acb1043ad74708e7b0092b
Moh Dev <73759551+MohamedCS1@users.noreply.github.com>
2023-02-08 00:31:19 +0100

    singIn and singUp design done

--- app/src/main/java/com/example/clientapplication/LoginActivity.kt
+++ app/src/main/java/com/example/clientapplication/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.clientapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.clientapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSingUp.setOnClickListener {
+            startActivity(Intent(this ,SingUpActivity::class.java))
+        }
     }
 }
